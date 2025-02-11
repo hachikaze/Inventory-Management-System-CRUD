c0b6615306debaff0a5938f88fa9b166b44e8245
Michael <michaeljohn.labton1@gmail.com>
2025-02-11 10:57:11 +0800

    Added SQL Fundamentals DB

---
+++ SQL_Fundamental.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: select * from employees
+LIMIT 0, 1000
+
+-- Date: 2025-02-11 10:16
+*/
+INSERT INTO `` (`EmployeeID`,`FirstName`,`LastName`,`DepartmentID`,`Salary`,`HireDate`) VALUES (6,'John','Doe',1,66550,'2020-03-15');
+INSERT INTO `` (`EmployeeID`,`FirstName`,`LastName`,`DepartmentID`,`Salary`,`HireDate`) VALUES (7,'Jane','Smith',2,60000,'2019-07-01');
+INSERT INTO `` (`EmployeeID`,`FirstName`,`LastName`,`DepartmentID`,`Salary`,`HireDate`) VALUES (8,'Alice','Brown',1,73205,'2021-06-23');
+INSERT INTO `` (`EmployeeID`,`FirstName`,`LastName`,`DepartmentID`,`Salary`,`HireDate`) VALUES (9,'Bob','Johnson',3,62000,'2018-09-12');
+INSERT INTO `` (`EmployeeID`,`FirstName`,`LastName`,`DepartmentID`,`Salary`,`HireDate`) VALUES (10,'Charlie','Wilson',2,58000,'2022-01-08');
